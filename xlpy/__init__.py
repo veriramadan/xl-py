1c95c564a9f81747f2fe6bb013ab596949ceb7e3
veriramadan <39357890+veriramadan@users.noreply.github.com>
2018-05-18 11:52:14 +0800

    Update __init__.py

--- xlpy/__init__.py
+++ xlpy/__init__.py
@@ -1,5 +1,5 @@
 from .config import Config
-from .client import Xl
+from .client import XL
 
 __copyright__      = 'Copyright 2018 Veri Ramadan'
 __version__        = '1.1.0'
@@ -7,4 +7,4 @@ __author__         = 'Veri Ramadan'
 __author_email__   = 'veridarkit25@gmail.com'
 __url__            = 'http://github.com/veriramadan'
 
-__All__            = ['Config','Xl']
+__all__            = ['Config','Xl']
